b717fec92f8d3a559016488d9c78846591114784
Victor A. Requena <svictoreq@gmail.com>
2019-02-02 14:05:02 -0400

    Agrega importacion de modulos locales

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-/**
- * *************
- * Comience aqui
- * *************
- */
+const math = require("./math");
+const greet = require("./greetings");
+const hello = require("./greetings/hello");
+
+console.log(math.add(4, 5));
+console.log(math.divide(4, 5));
+console.log(math.multiply(4, 5));
+console.log(math.substract(4, 5));
+
+console.log(greet.greet("Victor"));
+
+console.log(hello.sayHello("Freddy"))
