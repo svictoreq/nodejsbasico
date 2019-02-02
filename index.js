2af6ef1c31d3face09bb9238822c9b29a7115203
Victor A. Requena <svictoreq@gmail.com>
2019-02-02 18:00:46 -0400

    Agrega ejemplos de asincronia y sincronia

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+// const add = require("./src/addition");
+const sim = require("./src/intensive");
+
+// add.syncSum(1,2);
+// add.asyncSum(2,4);
+
+console.log("\x1b[35m%s\x1b[0m", "Sync simulation:");
+sim.simulateSync();
+
+console.log("\n");
+console.log("\x1b[36m%s\x1b[0m", "Async simulation:");
+sim.simulateAsync();
