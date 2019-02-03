83ae4bb4334c5d98ce0aafcb2ce9da904a4e9596
Victor A. Requena <svictoreq@gmail.com>
2019-02-02 21:48:27 -0400

    Agrega ejemplos de callbacks y promesas

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+// call.withCallback("Victor Requena", call.sync)
+
+call.withPromise('Victor Requena')
+  .then((nombre) => console.log(call.sync(nombre)));
--- src/call.js
+++ src/call.js
@@ -1,17 +1,19 @@
 module.exports = {
     sync: function(nombre) {
-        return "Hola ${nombre}! Bienvenido a Node.js";
+        return `Hola ${nombre}! Bienvenido a Node.js`;
     },
     withCallback: function(nombre, cb) {
+        console.log('\x1b[33m%s\x1b[0m', 'Comenzando llamada...')
         setTimeout(()=>{
-            cb(nombre)
+            console.log(cb(nombre))
         }, 5000);
     },
     withPromise: function(nombre) {
         return new Promise((resolve, reject)=>{
+            console.log('\x1b[36m%s\x1b[30m', 'Comenzando llamada...')
             setTimeout(()=>{
                 resolve(nombre);
-            }, 5000); 
+            }, 5000);
         })
     }
 }
