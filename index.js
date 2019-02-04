e3024896e0dca331a74300e27d08009e05a366d8
Victor A. Requena <svictoreq@gmail.com>
2019-02-04 11:31:51 -0400

    Implementa ejemplo de async/await

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+// call.withPromise("Victor", "Requena")
+//   .then((nombreCompleto) => {
+//     call.greet(nombreCompleto)
+//   });
+
+// Puede ser refactorizado a esto
+// usando Async/Await:
+async function callWithPromise() {
+  const nombreCompleto = await call.withPromise("Victor", "Requena");
+  call.greet(nombreCompleto);
+}
+
+callWithPromise();
