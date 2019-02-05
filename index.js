bfbbfe58f69b7fc649665206489a9027454b8080
Victor A. Requena <svictoreq@gmail.com>
2019-02-05 17:16:20 -0400

    Agrega ejercicio de uso del modulo chilp process

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require("child_process");
+
+const exec = (command) => {
+  cp.exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+      return;
+    }
+
+    if (stdout) console.log(`stdout:\n${stdout}`);
+    if (stderr) console.error(`stderr:\n${stderr}`);
+  });
+};
+
+// exec("ls");
+exec("node ./src/script.js --base=5");
