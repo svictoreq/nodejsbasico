e4994c341032b7ef6cb391e0148fe5cbe033308b
Victor A. Requena <svictoreq@gmail.com>
2019-02-07 12:38:26 -0400

    Agrega ejemplo de servidor basico en Express

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Estoy en la ruta /");
+});
+
+app.get("/app", (req, res) => {
+  res.send("Estoy en la ruta /app");
+});
+
+app.get("*", (req, res) => {
+  res.send("No sé en dónde estoy");
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
