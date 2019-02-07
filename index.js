f07d83d0e5b7a502830fa551efd530b0067a7ee4
Victor A. Requena <svictoreq@gmail.com>
2019-02-07 12:17:43 -0400

    Agrega ejemplo de servidor basico con el modulo http

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  fs.readFile("./resources/my_page.html", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.end(data);
+  })
+});
+
+server.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
