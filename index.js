cea394645d0f64b46509332480f5539954049e86
Victor A. Requena <svictoreq@gmail.com>
2019-02-15 13:49:13 -0400

    Agrega ejemplo de uso de socket.io

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require("http").Server();
+const io = require("socket.io")(server);
+const port = require("./config.json").SERVER_PORT;
+
+const banner =  `
+************************
+  Basic Node.js Course
+     Course Project
+   Tic Tac Toe Server
+************************
+Status: online
+Listening on port: ${port}
+`;
+
+io.on("connection", (socket) => {
+  socket.on("register", (user) => {
+    console.info(`User registered: ${user.name}`);
+  });
+});
+
+server.listen(port, () => {
+  console.info(banner);
+});
